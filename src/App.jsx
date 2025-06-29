58c384446b5266b198c24a88e8990f677df850b9
dayane-dmr <dayane.dmr@gmail.com>
2025-06-29 12:29:27 -0300

    feat: add message section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { ScrollTrigger } from "gsap/all";
 import NavBar from "./components/NavBar";
 import HeroSection from "./sections/HeroSection";
 import gsap from "gsap";
+import MessageSection from "./sections/MessageSection";
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -11,6 +12,7 @@ const App = () => {
     <main>
       <NavBar />
       <HeroSection/>
+      <MessageSection />
       <div className="h-dvh border border-red-500"></div>
     </main>
   )
