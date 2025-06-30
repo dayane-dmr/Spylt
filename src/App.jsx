d36c4f463048e827cb87f89c3031dea2448d220d
dayane-dmr <dayane.dmr@gmail.com>
2025-06-30 18:46:07 -0300

    add scrollSmoother plugin

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
-import { ScrollTrigger } from "gsap/all";
+import { ScrollSmoother, ScrollTrigger } from "gsap/all";
 import NavBar from "./components/NavBar";
 import HeroSection from "./sections/HeroSection";
 import gsap from "gsap";
 import MessageSection from "./sections/MessageSection";
 import FlavorSection from "./sections/FlavorSection";
+import { useGSAP } from "@gsap/react";
 
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 
 const App = () => {
+
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 3,
+      effects: true,
+    });
+  });
 
   return (
     <main>
       <NavBar />
-      <HeroSection/>
-      <MessageSection />
-      <FlavorSection />
-      <div className="h-dvh border border-red-500"></div>
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
+          <HeroSection />
+          <MessageSection />
+          <FlavorSection />
+          <div className="h-dvh border border-red-500"></div>
+        </div>
+      </div>
     </main>
   )
 }
--- src/components/FlavorSlider.jsx
+++ src/components/FlavorSlider.jsx
@@ -2,16 +2,21 @@ import { useGSAP } from "@gsap/react"
 import { flavorlists } from "../constants"
 import gsap from "gsap"
 import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 function FlavorSlider() {
     const sliderRef = useRef();
 
+    const isTablet = useMediaQuery({
+      query: "(max-width: 1024px)",
+    });
+  
     useGSAP(() => {
         const scrollAmount = sliderRef.current.scrollWidth - window.innerWidth;
 
-
-        const tl = gsap.timeline({
-            scrollTrigger:{
+        if(!isTablet){
+                    const tl = gsap.timeline({
+            scrollTrigger: {
                 trigger: ".flavor-section",
                 start: "2% top",
                 end: `+=${scrollAmount + 1100}px`,
@@ -20,10 +25,36 @@ function FlavorSlider() {
             },
         });
 
-        tl.to(".flavor-section",{
+        tl.to(".flavor-section", {
             x: `-${scrollAmount + 1100}px`,
             ease: "power1.inOut",
+        });
+        }
+
+
+
+
+        const titleTl = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".flavor-section",
+                start: "top top",
+                end: "bottom 80%",
+                scrub: true,
+            },
+        });
+
+        titleTl.to(".first-text-split", {
+            xPercent: -30,
+            ease: "power1.inOut",
         })
+            .to(".flavor-text-scroll", {
+                xPercent: -22,
+                ease: "power1.inOut",
+            }, "<")
+            .to(".first-text-split", {
+                xPercent: -10,
+                ease: "power1.inOut",
+            }, "<")
     });
 
     return (
