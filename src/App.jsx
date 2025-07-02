d4cb8a2faf05e79da78d0d04f4d4d57b2da9bc3d
dayane-dmr <dayane.dmr@gmail.com>
2025-07-02 13:42:01 -0300

    add benefit section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import MessageSection from "./sections/MessageSection";
 import FlavorSection from "./sections/FlavorSection";
 import { useGSAP } from "@gsap/react";
 import NutritionSection from "./sections/NutritionSection";
+import BenefitSection from "./sections/BenefitSection";
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 
@@ -27,6 +28,7 @@ const App = () => {
           <MessageSection />
           <FlavorSection />
           <NutritionSection />
+          <BenefitSection />
           <div className="h-dvh border border-red-500"></div>
         </div>
       </div>
