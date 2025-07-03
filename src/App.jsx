47ce1ab39a0a3bdd856c7eacafc4d5b0dec53cb5
dayane-dmr <dayane.dmr@gmail.com>
2025-07-03 08:26:55 -0300

    add footer section

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { useGSAP } from "@gsap/react";
 import NutritionSection from "./sections/NutritionSection";
 import BenefitSection from "./sections/BenefitSection";
 import TestimonialSection from "./sections/TestimonialSection";
+import FooterSection from "./sections/FooterSection";
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 
@@ -34,8 +35,7 @@ const App = () => {
             <BenefitSection />
             <TestimonialSection />
           </div>
-
-          <div className="h-dvh border border-red-500"></div>
+          <FooterSection />
         </div>
       </div>
     </main>
